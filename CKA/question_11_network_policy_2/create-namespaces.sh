f04d1394bfd31a94c45733a600ed109c1cbfacc2
KEMILA <kemila.hettinarayana@gmail.com>
2024-03-26 02:02:52 +0000

    added two shell scripts to create namespace and pods

---
+++ CKA/question_11_network_policy_2/create-namespaces.sh
@@ -0,0 +1,4 @@
+export k="kubectl"
+$k create namespace project-a
+$k create namespace project-b
+$k create namespace project-c
---
+++ CKA/question_11_network_policy_2/create-pods.sh
@@ -0,0 +1,4 @@
+export k="kubectl"
+$k apply -f application-pod.yaml
+$k apply -f frontend-pod.yaml
+$k apply -f backend-pod.yaml 
